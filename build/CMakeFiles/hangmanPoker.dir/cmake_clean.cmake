e964e72f19ce72b6c219a03090bead12ea813594
Matthew <deadwalker1228@gmail.com>
2024-01-12 14:34:37 -0800

    Initial commit

---
+++ build/CMakeFiles/hangmanPoker.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hangmanPoker.dir/main.cpp.obj"
+  "CMakeFiles/hangmanPoker.dir/main.cpp.obj.d"
+  "hangmanPoker.exe"
+  "hangmanPoker.exe.manifest"
+  "hangmanPoker.pdb"
+  "libhangmanPoker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hangmanPoker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
